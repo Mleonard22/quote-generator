84cac88284778af0295a3bb94a770392bb9e7b54
Mleonard <mwai94@gmail.com>
2024-06-28 16:53:14 -0400

    files for project

---
+++ Codecademy project/Messagejscode.js
@@ -0,0 +1,161 @@
+let quotes = `-The thought of affliction and wounds Is usually  deeper than the affliction itself( mike wardbey)
+-The value of life lies not in the length of days, but in the use we make of them; a man may live long yet live very little (Michel eyquem)
+-Until thought  is linked with purpose there is no intelligent accomplishment(James Allen)
+-What you focus on expands.(t. Harv eker )
+-bless that which you want.( huna Philosophy)
+-nothing  has meaning except for the meaning we give it(t harv eker)
+-No thought lives in your head rent free (Robert Allen)
+-The secret of success in life is for a man to be ready for his time when it comes (Benjamin Disraeli)
+-No Man can climb out beyond the limitations  of his own character (John Morley)
+-The only thing that walks back from the tomb  with the mourners and refuses to be buried is the character of a man this is true. what a man is survives him. It can never be buried
+-To lead yourself, use your head;To lead others, use your heart 
+-Success is not measured by what you’re leaving to, but  by what you are leaving behind (Chris Musgrove)
+-Nothing can resist the human will that will stake even its existence on it’s stated purpose (Benjamin Disraeli )
+-Any leader ship that teaches you to depend on another race is a leader ship that will enslave you(Marcus Garvey)
+-it’s your decisions and not your conditions that determine your destiny (tony Robbins)
+-Good communication is the bridge between confusion and clarity (Eric Thomas)
+-Greatness is remembering in the dark what the creator told you in the light (Eric Thomas)
+-We are what we think. all that we are arises with our thoughts. with our thoughts, we make our world (Buddha)
+-If you fail to plan you prepared to fail 
+-It Is not enough to have a good mind; the main thing is to use it well (Rene Decartes)
+-experience is not what happens to a man; it is what a man does with what happens to him. (Aldous Huxley)
+-Tough Times create strong men, strong men create easy times, easy times create weak men, Weak men create tough times.  Many will not understand, but you have to create warriors.
+-He who asks questions cannot avoid the answers. (Cameroon proverb)
+-Your past does not equal your future
+-Success is the by product of failure (Ken Brown)
+-All communication is either a loving response or a cry for help. (A course in miracles)
+-Sometimes we need to trust that our disappointments may truly be opportunities in disguise.(tony robbins)
+-Habit is either the best of servants or the worst of masters.(Nathaniel Emmons )
+-No doesnt mean no it means next opportunity
+-Success is not about you but its up to you
+-Done is better than perfect 
+-Nothing splendid has ever been achieved except by those who dared believe that something inside of them was superior to circumstance.(Bruce Barton)
+-I am the source of all my emotions. Nothing and no one can change how I feel except me. If I find myself in reaction to anything, I can change it in a moment.
+-A man’s character is his guardian divinity(Heraclitus)
+-The world makes way for the man who knows where he’s going 
+-as long as we structure our lives in a way where our happiness is dependent upon something we cannot control, then we will experience pain.(tony Robbins)
+-so if you ever feel angry or upset with someone, remember, it’s your rules that are upsetting you, not their behavior.( tony Robbins)
+-A free man  is free to acknowledge his fears, without hiding them, or hiding from them.(David Deida)
+-Nothing great will ever be achieved without great men, and men are great only if they are determined to be so.(Charles de Gaulle)
+-The only way a relationship will last is if you see your relationship as a place that you go to give, and not a place that you go to take.(tony Robbins)
+-The key to wealth is to be more valuable.(tony Robbins)
+-Don’t confuse movement for progress
+-Pressure doesn’t create negative behavior… Think of yourself as an orange… The only thing that comes out when the pressure is applied is what’s already inside the orange.(Wayne Dyer)
+-If you focus on what you left behind, you will never see what lies ahead. (Gusteau)
+-The great man is he that does not lose his child’s-heart.(Mencius)
+-Life is Cumulative. (Tony Robbins)
+-Trust your intuition and give of your heart; you’ll be surprised at the miracles that will occur.(Tony Robbins)
+-Strive for balance rather than perfection.(Tony Robbins)
+-You alone are responsible for what you do, don’t do, or how you respond to what’s done to you.(Darren Hardy)
+-We are what we repeatedly do. (Aristotle)
+-Your choices are only meaningful when you connect them to your desires and dreams. (Darren Hardy)
+-Whatever you vividly imagined, ardently desire, sincerely believe, and enthusiastically act upon..Must inevitably come to pass! (Paul J. Meyer)
+-Success is something you attract by the person you become. (Daren Hardy)
+-Your life comes down to this formula: 
+You -> choice (decision) + behavior (action) + habit (repeated action) + compounded (time) = Goals (Darren Hardy  )
+-Live Full and Die Empty. (Les Brown)
+-When you lack the motivation have the discipline to push through
+-The ultimate measure of a man is not where he stands in moments of comfort and convenience, but where he stands at times of challenge.(Martin Luther King Jr)
+-In order to grow, your identity must be whatever is required to grow.(Alex Hormozi)
+-The most convicted person will always win the fight. (Alex Hormozi)
+-Remember that you get in life what you tolerate. Put another way, you will get in life what you accept and expect you are worthy of. (Darren Hardy)
+-Find the line of expectations and then exceed it.(Darren Hardy)
+-Motivation without action leads  to self-delusion.(Darren Hardy)
+-The only person you are destined to be come is the person you decide to be. (Ralph Waldo Emerson)
+-What’s wrong is always available if you look for it but so is what’s right.(Tony Robbins)
+-Casualness creates casualties.(Jim Rohn)
+-Let worry alarm you, but don’t let it conquer you.(Jim Rohn)
+-Success is the best revenge. (Loida Lewis) 
+-Provide more service than you get paid for(Les Brown)
+-Creativity thinks up new things. Innovation does new things.(Theodore Levitt)
+-Blessings don’t come to you, blessings flow through you. (Inky Johnson)
+-The difference between great people and everyone else is that great people create their lives actively, while everyone else is created by their lives, passively waiting to see where life takes them next.(Michael Gerber)
+-Get up otherwise your comfort zone will destroy you. 
+-Life is too short to be little (Disraeli)
+-Remind yourself regularly that you are better than you think you are.(David Schwartz)
+-Action cures fear.(David Schwartz)
+-As a man thinketh in his heart so is he.(James Allen)
+-Act is the blossom of thought, and joy and suffering are its fruits.(James Allen) 
+-Man Is the master of thought, the molder of character, and the maker and shaper of condition, environment, and destiny.(James Allen) 
+-The Outer world of circumstance shapes itself to the inner world of thought, and both pleasant and unpleasant external conditions are factors, which make for the ultimate good of the individual.(James Allen)
+-Men do not attract that which they want, but that which they are.(James  Allen)
+-To think confidently, act confidently.
+-Look at things not as they are, but as they can be. Visualization adds value to everything. A big thinker always visualizes what can be done in the future. He isn’t stuck with the present. (David Schwartz)
+-The genius thing that we did was, we didn't give up.(Jay-Z)
+-Make someone feel important and he cares about you. And when he cares about you, he does more for you.(David Schwartz)
+-Put service first and money takes care of itself.(David Schwartz)
+-Always Give people more than they expect to get.(David Schwartz)
+-Think right toward people.(David Schwartz)
+-To wait for the perfect set of conditions is to wait forever.(David Schwartz)
+-Nothing comes merely by thinking about it.(John Wanamaker)
+-Don't waste time and energy being discouraged. Don't berate yourself. Plan to win next time.
+-How you think when you lose determines how long it will be until you win.(David Schwartz)
+-We can turn setbacks into victories. Find the lesson, apply it, and then look back on defeat and smile.(David Schwartz)
+-Defeat is only a state of mind, and nothing more.(David Schwartz)
+-Have the courage to face your faults.
+-Desire, when harnessed , is power. Failure to follow desire, to do what you want to do most, pays the way to mediocrity. (David Schwartz)
+-The person determined to achieve maximum success learns the principal that progress is made one step at a time. (David Schwartz)
+-Commit this question to memory and use it to evaluate everything you do. “ Will this help take me where I want to go? “ If the answer is no, back off; if yes, press ahead.(David Schwartz)
+-remember: if you think you are weak, you are. If you think you’re inadequate, you are. If you think your second class, you are.(David Schwartz)
+-A wise man will be Master of His Mind A Fool will be its Slave .(Publilius Syrus)
+-Everyday in every way I am getting better and better.
+-Be still, and know that I am god
+-Procrastination is not a problem. Procrastination is always the symptom to a problem (Myron Golden)
+-You can no more change your environment or world by destroying things than you can your reflection by destroying the mirror.(Neville Goddard)
+-Live as though it was true.(Neville Goddard)
+-The truth can always be expressed at a child’s level of understanding. It just takes some thought. (The Celestine prophecy)
+-If you are pained by external things, it is not they that disturb you, but your own judgment of them. And it is in your power to wipe out that judgment now. (Marcus Aurelius)
+-Don’t fake it till you make it, project it till you perfect it  (Myron Golden)
+-Transmutation, not  presumptuous denial , is the weapon of the master
+-Don’t start the day till you have it finished.(Jim Rohn)
+-We better spend some time preparing for our future because we are going to spend the rest of our lives there.
+-Selling is helping, not convincing.(Mark Cuban)
+-On the great days be great, and on the bad days be greater.(Eric Thomas)
+-Make The rest of your life the best of your life. (Eric Thomas)
+-Enjoyment appears at the boundary between boredom and anxiety, when the challenges are just balanced with a persons capacity to act.(mihaly csikszentmihalyi) 
+-Assume nothing will happen as easily or smoothly as you hope (Steve magnus)
+-Entertainment and distraction is the enemy of creation and learning. They will keep you in mediocrity.(Benjamin Hardy) 
+-Give a 120% or don’t do it.(Eric Thomas)
+-Our behavior is heavily dependent on how we interpret the events that happen to us, not necessarily the objective reality of the events themselves.(James Clear)
+-The best is the enemy of the good(Voltaire)
+-Your thoughts are your reactions to the incident always.(Eric butterworth )
+-Men are men and life is life, and we must deal with them as they are; and if we want to change them, you must deal with them in the form in which they Exist.(Richard Wright.)
+-never be a minion, always be an owner. (Cornelius Vanderbilt)`
+
+let quoteArr = quotes.split('\n')
+
+
+
+
+
+
+function quoteGenerator () {
+    let final = Math.floor(Math.random() * quoteArr.length);
+    let word = quoteArr[final];
+    let lastTime = [];
+    for (let i = 0; i < word.length; i++) {
+     if (word[i] == '-' || word[i] == '(' || word[i] == ')' ) {
+    
+        continue}
+    lastTime.push(word[i])
+    } 
+    
+    let x = lastTime.join('')
+   
+
+    let forQuotes = document.getElementById('forQuotes');
+    
+    forQuotes.textContent = x;
+
+
+} 
+
+
+
+
+
+let theButton = document.querySelector('button')
+
+
+theButton.addEventListener('click', quoteGenerator)
+
